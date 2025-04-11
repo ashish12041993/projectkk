79e3383a720ed5fb0d97c886fc2e320a0cea2654
ashish12041993 <ashish.mnnit777@gmail.com>
2025-04-11 11:06:20 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+def create_ec2_instance():
+    try:
+        response = ec2.run_instances(
+            ImageId='ami-0abcdef1234567890',  
+            InstanceType='t2.micro',          
+            KeyName='your-key-pair-name',     
+            MinCount=1,
+            MaxCount=1,
+            SecurityGroupIds=['sg-0123456789abcdef0'],  
+            SubnetId='subnet-0123456789abcdef0',       
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {'Key': 'Name', 'Value': 'MyBoto3Instance'}
+                    ]
+                }
+            ]
+        )
